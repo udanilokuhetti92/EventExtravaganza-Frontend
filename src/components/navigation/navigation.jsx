9e88ce1e9d7d8479cd927e8610c5aa1c90afbb39
TharushaDenuwan <tharushadenuwan35@gmail.com>
2025-02-03 00:34:50 +0530

    working on budget filtering feature

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import '../navigation/navigation.css'
 
-export default function navigation() {
+export default function () {
   return (
-    <div>navigation</div>
+    <div className='nav'>
+        <div className='navbox'>
+            <h1 className='h1'>Extravaganza</h1>
+
+            <ul className='list'>
+                <li className='li'>Budget <br/> Filtering</li>
+                <li className='li'>Search <br/> Venues</li>
+                <li className='li'>Customer <br/> Support</li>
+                <li className='list2'>Notifications</li>
+                <li className='list2'>Profile</li>
+            </ul>
+        </div>
+        <hr style={{ border: "2px solid black" }} />
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import BudgetFiltering from './budget_filtering/budget_filtering'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+    <BudgetFiltering/>
   </StrictMode>
 )

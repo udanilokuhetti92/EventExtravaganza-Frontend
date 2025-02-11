52687328bc866da41226e025849a274a102cc720
Udani Lokuhetti <udani.20232492@iit.ac.lk>
2025-02-11 19:04:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {chatbot} from './chatbot/chatbot'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {chatbot}
   </StrictMode>
 )

7e4ce1e71a7831b97072346822180fb276b832cc
Sanuka <sanuka.20232509@iit.ac.lk>
2025-02-11 17:15:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import {chatbot} from './chatbot/chatbot'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
+    {chatbot}
   </StrictMode>
 )

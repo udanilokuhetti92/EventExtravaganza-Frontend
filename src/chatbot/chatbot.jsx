6351d45b53562195adb4a622d002a87a0e3b99ce
Sanuka <sanuka.20232509@iit.ac.lk>
2025-02-12 19:54:31 +0530

    Update chatbot.jsx

--- src/chatbot/chatbot.jsx
+++ src/chatbot/chatbot.jsx
@@ -2,6 +2,22 @@ import {useState} from 'react'
 
 export default function chatbot() {
   return (
-    <div>chatbot</div>
-  )
+    <div className="chatbot-container">
+      <div className="messages">
+        {messages.map((msg,index) =>(
+          <div key={index} className={`message ${msg.role}`}>
+            {msg.content}
+          </div>
+        ))}
+        <div ref={messagesEndRef}/>
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={input}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
 }

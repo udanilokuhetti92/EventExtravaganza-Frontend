8cd9c9dc59a7fab7beb755207f771d386c19f1ff
Ashika SATHSARANI <ashika.20230077@iit.ac.lk>
2025-02-17 19:57:11 +0530

    updated navigation

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../navigation/navigation.module.css';
-import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
@@ -9,15 +8,21 @@ export default function Navigation() {
         <h1 className={styles.h1}>Extravaganza</h1>
 
         <ul className={styles.list}>
-          <li className={styles.li}><Link to="/budget-filtering">Budget <br/> Filtering</Link></li>
-          <li className={styles.li}><Link to="/search-venues">Search <br/> Venues</Link></li>
-          <li className={styles.li}><Link to="/customer-support">Customer <br/> Support</Link></li>
-          <li className={styles.list2}><Link to="/notifications">Notifications</Link></li>
-          <li className={styles.list2}><Link to="/profile">Profile</Link></li>
-          <li className={styles.list2}><Link to="/ratings">Ratings</Link></li> {/* Added Ratings */}
+          <li className={styles.list2}>Home</li>
+          <li className={styles.list2}>About</li>
+          <li className={styles.list2}>
+            Services
+            <div className={styles.dropdown}>
+              <li>Budget Filtering</li>
+              <li>Search Venues</li>
+              <li>Chat Bot</li>
+            </div>
+          </li>
+          <li className={styles.list2}>Inbox</li>
+          <b><li className={styles.list3}>Profile</li></b>
         </ul>
       </div>
-      <hr style={{ border: "1px solid black" }} />
+      <hr style={{ border: "2px solid black" }} />
     </div>
   );
 }

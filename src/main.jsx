fd34f46ffb5a7d16c29bc58217b081f2e6aff6d5
DisandiUmani <disandiumani24@gmail.com>
2025-02-17 15:40:01 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import BudgetFiltering from './budget_filtering/budget_filtering'
 import Home_page from './components/home_page/home_page'
 import Event_Organizer_Login from './components/log_in/event_organizer_login/event_organizer_login'
 import Event_Planner_Login from './components/log_in/event_planner_login/event_planner_login'
+import EventInvitation from "./invitation_send/invitation_send";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Home_page/> */}
-    <BudgetFiltering/>
+    {/* <BudgetFiltering/> */}
+    <EventInvitation/>
     {/* <Event_Organizer_Login/> */}
     {/* <Event_Planner_Login/> */}
     

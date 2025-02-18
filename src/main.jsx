1ba1d75b7fb6ff1913a6f387ed1291354a8119a5
Sanuka <sanuka.20232509@iit.ac.lk>
2025-02-18 09:35:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Chatbot from './chatbot/chatbot'
-import Navigation from './navigation/navigation'
+import Navigation from './components/navigation/navigation'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {<Chatbot /> }
     {<Navigation />}
+    {<Chatbot /> }
+    
     
     
   </StrictMode>

395d4f56ce481bf213acca445e04d3bfc4e5c1c2
udani lokuhetti <udani.20232492@iit.ac.lk>
2025-02-20 21:00:00 +0530

    feat: Connect to the backend

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,13 @@ import Checklist from './checklist/checklist'
 import Home_page from './components/home_page/home_page'
 import Event_Organizer_Login from './components/log_in/event_organizer_login/event_organizer_login'
 import Event_Planner_Login from './components/log_in/event_planner_login/event_planner_login'
+import Navigation from './components/navigation/navigation'
+import Footer from './components/footer/footer'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Home_page/> */}
-    {/*<BudgetFiltering/>*/}
-    {<Checklist/>}
-    {/* <Event_Organizer_Login/> */}
-    {/* <Event_Planner_Login/> */}
-    
+   
+    { <Checklist /> }
   </StrictMode>
 )

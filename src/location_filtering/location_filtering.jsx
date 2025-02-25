70bf0c1902cdee7910e4863220ad3a56124b9892
PasiiSuwahas <pasan.20232191@iit.ac.lk>
2025-02-25 08:12:56 -0800

    change the template and the Map fetures

--- src/location_filtering/location_filtering.jsx
+++ src/location_filtering/location_filtering.jsx
@@ -7,6 +7,8 @@ export default function location_filtering() {
   return (
    
     <div className='box1'>
+
+      <div className='container'>
           <Navigation/>
     
           <h3 className='h3'><span className='green-pipe'>|</span> Extravaganza unit</h3>
@@ -15,15 +17,16 @@ export default function location_filtering() {
           <p className='p2'>Finding the perfect event planner within your area Simply <br />
              enter your
              Location, and our system will instantly display a list of<br />available event planners that match  your 
-             price preferences.  This feature ensures <br /> that you can plan your dream event without exceeding your financial limits,<br /> 
-             making event planning hassle-free and affordable.</p>
+             in your area.
+            </p>
           <div className='google-map'> 
           <GoogleMapComponent/>
           </div>   
     
          
-          <span className='span1'></span><input className='input1' type="number" placeholder='Enter Your Location'/>    
+          <span className='span1'>Location:</span><input className='input1' type="number" placeholder='Enter Your Location'/>    
           <button className='button1'>Search</button>  
+          </div>
         </div>
    
     
--- src/map/google_map_component.jsx
+++ src/map/google_map_component.jsx
@@ -8,14 +8,25 @@ export default function GoogleMapComponent() {
   };
 
   const center = {
-    lat: 37.7749, // Latitude (San Francisco)
-    lng: -122.4194, // Longitude
+    lat: 7.290572, // Latitude (San Francisco)
+    lng: 80.633728, // Longitude
   };
+
+  const points = [
+    { lat: 37.7749, lng: -122.4194 },
+    { lat: 37.7849, lng: -122.4294 },
+    { lat: 37.7649, lng: -122.4094 },
+    { lat: 37.7549, lng: -122.4394 },
+  ];
+  
 
   return (
     <LoadScript googleMapsApiKey="AIzaSyCMZnyi5d0x6b-x0zvDPLXPogYe9PbstXQ">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-      <MarkerF position={{ lat: 37.7749, lng: -122.4194 }} />
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={9}>
+      {/* <MarkerF position={{ lat: 37.7749, lng: -122.4194 }} /> */}
+      {points.map((point, index) => (
+          <MarkerF key={index} position={point} />
+        ))}
       </GoogleMap>
     </LoadScript>
   );

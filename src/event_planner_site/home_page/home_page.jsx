0c2a9d749c2ec04b786867fe118b6a3b9ee70f60
TharushaDenuwan <tharushadenuwan35@gmail.com>
2025-02-28 03:10:59 +0530

    updated planner home page

--- src/event_planner_site/home_page/home_page.jsx
+++ src/event_planner_site/home_page/home_page.jsx
@@ -7,17 +7,18 @@ export default function home_page() {
     <div>
         <div className={styles.main1}>
             {<Navigation/>}
-            <div className={styles.box11}> 
-                <div className={styles.box1}>
-                    <h2 className={styles.h1}>WELCOME TO EXTRAVAGANZA</h2>
-                    <button class={styles.button}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
-                        </svg>
-                        <div class={styles.text}>
-                            Go To Your Projects
-                        </div>
-                    </button>
+            <div className={styles.box1}> 
+                <p className={styles.p1}>Your Projects</p>
+                <p className={styles.p2}>Here are your projects you currently working on</p>
+                <div className={styles.box2}>
+                    <div className={styles.box3}>
+                        <p className={styles.p3}>Current Projects</p>
+                        <hr style={{ border: "1px solid #0073e5" }} />
+                    </div>
+                    <div className={styles.box3}>
+                        <p className={styles.p3}>Project History</p>
+                        <hr style={{ border: "1px solid #0073e5" }} />
+                    </div>
                 </div>
             </div>
         </div>

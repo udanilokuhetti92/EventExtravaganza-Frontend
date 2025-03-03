d32c40eab9249be09860b96aeed93e0a86ee58fb
TharushaDenuwan <tharushadenuwan35@gmail.com>
2025-03-03 15:11:31 +0530

    changed main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Routes, Route } from 'react-router-dom';
-import BudgetFiltering from './budget_filtering/budget_filtering';
-import HomePage from './components/home_page/home_page';
-import EventOrganizerLogin from './components/log_in/event_organizer_login/event_organizer_login';
-import EventPlannerLogin from './components/log_in/event_planner_login/event_planner_login';
-import Navigation from './components/navigation/navigation';
-import EventPlannerProfile from './components/event_planner_profile/event_planner_profile';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import BudgetFiltering from './budget_filtering/budget_filtering'
+import Checklist from './checklist/checklist'
+import Home_page from './components/home_page/home_page'
+import Home from './components/home_page/home'
+import Event_Organizer_Login from './components/log_in/event_organizer_login/event_organizer_login'
+import Event_Planner_Login from './components/log_in/event_planner_login/event_planner_login'
+import EventInvitation from "./invitation_send/invitation_send";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Event_Organizer_Signin from './components/sign_in/event_organizer_signin/event_organizer_signin'
+import Event_Planner_Signin from './components/sign_in/event_planner_signin/event_planner_signin'
+import Navigation from './event_planner_site/navigation/navigation'
+import Home_PAGE from './event_planner_site/home_page/home_page'
+import Packages from './event_planner_site/packages/packages'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,12 +21,11 @@ createRoot(document.getElementById('root')).render(
       {/* {<Event_Organizer_Signin/>} */}
       {/* {<Event_Planner_Signin/>} */}
       {/* {<BudgetFiltering/>} */}
-      {/* {<Home_PAGE/>} */}
+      {/* {<Home_Page/>} */}
       {/* {<Navigation/>} */}
       {/* {<Packages/>}  */}
       {/* {<Event_Organizer_Signin/>} */}
       <Routes>
-
         <Route path='/' element={<Home_page/>} />
         <Route path='/organizer_login' element={<Event_Organizer_Login/>} />
         <Route path='/planner_login' element={<Event_Planner_Login/>} />
@@ -30,12 +35,15 @@ createRoot(document.getElementById('root')).render(
         <Route path='/Budget_Filtering' element={<BudgetFiltering/>} />
         <Route path='/Home_PAGE' element={<Home_PAGE/>} />
         <Route path='/Packages' element={<Packages/>} />
-        <Route path='/Checklist' element={<Checklist/>} />
+
+
 
         {/* { <EventInvitation/> }
         { <BudgetFiltering/> }
         { <Checklist/> } */}
       </Routes>
     </Router>
+
+    
   </StrictMode>
-);
+)
